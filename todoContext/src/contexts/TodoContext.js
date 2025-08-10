e641a0365d47b1f36760d36cb9ddcf8740b22f13
Saurabh Kumar Singh <imsaurabhsinha51@gmail.com>
2025-08-10 19:24:01 +0530

    local storage with context api

---
+++ todoContext/src/contexts/TodoContext.js
@@ -0,0 +1,21 @@
+import { useContext, createContext } from "react";
+
+export const TodoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            todo: "Learn React",
+            completed: false,
+        }
+    ],
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleTodo: (id) => {},
+});
+
+export const TodoProvider = TodoContext.Provider;
+
+export const useTodo = () => {
+    return useContext(TodoContext);
+}
